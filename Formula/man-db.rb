3abbfede67b31dbc4133c860d154a6417f645cc5
Bob W. Hogg <rwhogg@linux.com>
2017-02-14 19:26:49 -0800

    man-db 2.7.6.1
    Closes Linuxbrew/homebrew-extra#6.
    
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- Formula/man-db.rb
+++ Formula/man-db.rb
@@ -1,9 +1,8 @@
 class ManDb < Formula
   desc "Unix documentation system"
   homepage "http://man-db.nongnu.org/"
-  url "https://download.savannah.gnu.org/releases/man-db/man-db-2.6.7.1.tar.xz"
-  sha256 "8d65559838fccca774e3ef7c15c073180c786e728785c735e136297facca41fc"
-  # tag "linuxbrew"
+  url "https://download.savannah.gnu.org/releases/man-db/man-db-2.7.6.1.tar.xz"
+  sha256 "08edbc52f24aca3eebac429b5444efd48b9b90b9b84ca0ed5507e5c13ed10f3f"
 
   bottle do
     sha256 "4437811b171f2e4f9831a87a7c435dab3224011506246628449700838f7869fe" => :x86_64_linux
@@ -28,7 +27,9 @@ class ManDb < Formula
     system "./configure",
       "--disable-dependency-tracking",
       "--disable-silent-rules",
-      "--prefix=#{prefix}"
+      "--prefix=#{prefix}",
+      "--with-systemdtmpfilesdir=#{lib}/tmpfiles.d",
+      "--disable-setuid"
     system "make", "install"
   end
 
