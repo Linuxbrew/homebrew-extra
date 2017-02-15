02c4b4a231cb71b5b94edcb21a0ae8d81edf76ef
Bob W. Hogg <rwhogg@linux.com>
2017-02-14 19:13:17 -0800

    libpipeline: import from core
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

---
+++ Formula/libpipeline.rb
@@ -0,0 +1,22 @@
+class Libpipeline < Formula
+  desc "Library for manipulating pipelines of subprocesses"
+  homepage "http://libpipeline.nongnu.org/"
+  url "https://download.savannah.gnu.org/releases/libpipeline/libpipeline-1.3.1.tar.gz"
+  sha256 "5cad1b446f135ec3800d32c8c951a1114f4c438609a4c52b262c30301bc8e692"
+  # tag "linuxbrew"
+
+  head do
+    url "git://git.savannah.nongnu.org/libpipeline.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
+  def install
+    system "./configure",
+      "--disable-debug",
+      "--disable-dependency-tracking",
+      "--disable-silent-rules",
+      "--prefix=#{prefix}"
+    system "make", "install"
+  end
+end
