35d55c055432741d21324591ca4a88286c45956d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-02-22 05:15:48 +0000

    libpipeline: update 1.4.1 bottle for Linuxbrew.

--- Formula/libpipeline.rb
+++ Formula/libpipeline.rb
@@ -5,8 +5,7 @@ class Libpipeline < Formula
   sha256 "da46d7b20163aadb9db2faae483f734e9096a7550c84b94029abeab62dd1b9ee"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "683eb078924cbf5e117dea4037ee13847e341781e0bfe042c1966695a47056fb" => :x86_64_linux
+    sha256 "1aadf9bcd7d5a8d974599654e9359163fda482caab1bfc104e97e7a8cbd7f1e7" => :x86_64_linux
   end
 
   head do
