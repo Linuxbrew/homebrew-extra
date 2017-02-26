3b373b57e0387c052c50c984bbcec783416b3ee6
Bob W. Hogg <rwhogg@linux.com>
2017-02-26 07:23:35 -0800

    strace 4.16
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>
    
    Closes Linuxbrew/homebrew-extra#9.

--- Formula/strace.rb
+++ Formula/strace.rb
@@ -1,10 +1,9 @@
 class Strace < Formula
   desc "Diagnostic, instructional, and debugging tool for the Linux kernel"
   homepage "https://strace.io/"
-  url "https://github.com/strace/strace/archive/v4.15.tar.gz"
-  sha256 "e5fc45bdf72a36e621868ff4c67d645a5087d3d2f31571ad6bca21c64b5ac870"
+  url "https://github.com/strace/strace/archive/v4.16.tar.gz"
+  sha256 "4a6dcf63f2b3e49b840380396d2a8a5a4fb30ca12f953d82c16236bfe620e45a"
   head "https://github.com/strace/strace.git"
-  # tag "linuxbrew"
 
   bottle do
     cellar :any_skip_relocation
