6ecf667b5f66dc3935ed33942a09031da7471c43
George Gastaldi <gegastaldi@gmail.com>
2017-06-21 14:08:13 -0300

    Update minishift.rb

--- Formula/minishift.rb
+++ Formula/minishift.rb
@@ -1,9 +1,10 @@
 class Minishift < Formula
-  desc "Runs a single-node OpenShift cluster inside a VM in Linux"
-  homepage "https://minishift.io/"
-  url "https://github.com/minishift/minishift/releases/download/v1.1.0/minishift-1.1.0-linux-amd64.tgz"
   version "1.1.0"
   sha256 "6eb46670dd7df1696db20137cfc9bff6a84d42d9e846a329746c53a36163bb90"
+
+  desc "Runs a single-node OpenShift cluster inside a VM in Linux"
+  homepage "https://minishift.io/"
+  url "https://github.com/minishift/minishift/releases/download/v#{version}/minishift-#{version}-linux-amd64.tgz"
 
   bottle :unneeded
 
