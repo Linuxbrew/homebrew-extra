f02f2c341ba1d45671a87b29756e4707ffde4429
Michka Popoff <michkapopoff@gmail.com>
2018-01-24 20:48:46 +0100

    iotop: Depend on Python formula

--- Formula/iotop.rb
+++ Formula/iotop.rb
@@ -3,14 +3,14 @@ class Iotop < Formula
   homepage "http://guichaz.free.fr/iotop/"
   url "http://guichaz.free.fr/iotop/files/iotop-0.6.tar.bz2"
   sha256 "3adea2a24eda49bbbaeb4e6ed2042355b441dbd7161e883067a02bfc8dcef75b"
+  revision 1
   head "git://repo.or.cz/iotop.git"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "37406667cc6ad1a504bf24e194b986ec2c45799c36c8b87e090a20a5c1be6bb1" => :x86_64_linux
   end
 
-  depends_on :python
+  depends_on "python"
 
   def install
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
