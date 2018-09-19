b8a8bd679814a17ff134cde4ef145b40e4f7b511
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-09-19 21:54:39 +0000

    singularity: add 2.6.0 bottle.

--- Formula/singularity.rb
+++ Formula/singularity.rb
@@ -4,6 +4,11 @@ class Singularity < Formula
   url "https://github.com/singularityware/singularity/releases/download/2.6.0/singularity-2.6.0.tar.gz"
   sha256 "7c425211a099f6fa6f74037e6e17be58fb5923b0bd11aea745e48ef83c488b49"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-extra"
+    sha256 "9350470e86dfa8e1fe057b29b26f051734ba257297b212fc037e3bbb8b7022ba" => :x86_64_linux
+  end
+
   depends_on "python@2" => :build
   depends_on "libarchive"
   depends_on "squashfs"
