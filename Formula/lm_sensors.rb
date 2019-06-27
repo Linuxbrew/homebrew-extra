f8dd91d366c9ee376d90b664e030f593aa301a85
Maxim Belkin <maxim.belkin@gmail.com>
2019-06-27 19:14:15 +0000

    lm_sensors: specify version explicitly

--- Formula/lm_sensors.rb
+++ Formula/lm_sensors.rb
@@ -2,6 +2,7 @@ class LmSensors < Formula
   desc "Tools for monitoring the temperatures, voltages, and fans"
   homepage "https://github.com/groeck/lm-sensors"
   url "https://github.com/lm-sensors/lm-sensors/archive/V3-5-0.tar.gz"
+  version "3.5.0"
   sha256 "f671c1d63a4cd8581b3a4a775fd7864a740b15ad046fe92038bcff5c5134d7e0"
 
   bottle do
