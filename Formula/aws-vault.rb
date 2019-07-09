a3d8daad2d51f4af45d4ad2cbe8c5980b7e5b970
Issy Long <me@issyl0.co.uk>
2019-07-09 19:08:01 +0100

    aws-vault: Fix empty version string
    - In issue 59, I noticed that `aws-vault --version` didn't output anything.
    - This is because in the
      [Makefie](https://github.com/99designs/aws-vault/blob/master/Makefile#L2),
      it tries to do a `git describe --tags` which doesn't work because the
      tarball is not a Git repo, which results in:
      ```
      fatal: not a git repository (or any of the parent directories): .git
      go build -o aws-vault -ldflags="-X main.Version= -s -w" .
      ```
    - Instead, set a `flags` variable that's passed into `go build` that
      includes the version string.
    - This is, I think, preferable to setting this formula up to pull from a
      GitHub tag, as that needs more updating when a tag is released because
      `tag`s require `revision`s to pass the audit.
    
    aws-vault: Add `revision 1` to rebuild the bottle
    
    - The formula version hasn't had any changes, but the formula build step
      has. To rebuild the bottle and have people's bottled formulae upgrade
      seamlessly, bump the `revision`.
    
    Fixes #59.
    Closes #60.
    
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/aws-vault.rb
+++ Formula/aws-vault.rb
@@ -3,6 +3,7 @@ class AwsVault < Formula
   homepage "https://github.com/99designs/aws-vault"
   url "https://github.com/99designs/aws-vault/archive/v4.6.1.tar.gz"
   sha256 "7e4a6195fdbdf4a4e9bac4ce790d7463bba03da0cdcc81fcbca3dd9415027f1e"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -15,7 +16,9 @@ class AwsVault < Formula
     ENV["GOOS"] = "linux"
     ENV["GOARCH"] = "amd64"
 
-    system "make", "build"
+    flags = "-X main.Version=#{version} -s -w"
+
+    system "go", "build", "-ldflags=#{flags}"
     bin.install "aws-vault"
 
     zsh_completion.install "completions/zsh/_aws-vault"
