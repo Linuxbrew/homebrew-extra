3cf997c4dcc953b7b1f0c43ce4d088c7a974c035
Issy Long <me@issyl0.co.uk>
2019-08-09 11:49:53 +0100

    aws-vault 4.6.3

--- Formula/aws-vault.rb
+++ Formula/aws-vault.rb
@@ -1,8 +1,8 @@
 class AwsVault < Formula
   desc "Securely store and access AWS credentials in development environments"
   homepage "https://github.com/99designs/aws-vault"
-  url "https://github.com/99designs/aws-vault/archive/v4.6.2.tar.gz"
-  sha256 "56e6cc997acffd53ab188b8759d7e119f0c745a806f137fc964a2d0ee6353b59"
+  url "https://github.com/99designs/aws-vault/archive/v4.6.3.tar.gz"
+  sha256 "89428b99568d67335c5a9a4f74bdc7f4511864aa1dd11a07e7912588bcaa2716"
 
   bottle do
     cellar :any_skip_relocation
